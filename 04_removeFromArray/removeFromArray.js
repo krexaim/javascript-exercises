49e3f924bc02f6361bdfcf23f79b3f82d1a56bd4
krexaim <krexaim@gmail.com>
2022-07-01 16:39:26 -0300

    attempt

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item) {
+    //create array of arguments (1, length)
+    let args = [];
+    let newArray = [];
 
-};
+    for (let i = 1; i < arguments.length; i++) {
+        args[i] = arguments[i];
+        }
+    for (let i = 0; i < args.length; i++) {
+        if (!array.includes(args[i])) {
+            newArray.push(array[i])
+        }
+    }
+    return newArray;
+}
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
