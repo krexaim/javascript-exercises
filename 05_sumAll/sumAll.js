9ccaff836496866be13e6b2ebab20d454df2f78c
krexaim <krexaim@gmail.com>
2022-07-04 16:21:17 -0300

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    //error cases
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    
+    // buffer to switch in case first number is higher
+    if (a > b) {
+        let high = a;
+        let low = b;
+        b = high;
+        a = low;
+    };
+
+    for (let i = a; i < b - a + 2; i++) {
+        sum += i;
+    };
+
+    return sum;
 };
 
 // Do not edit below this line
