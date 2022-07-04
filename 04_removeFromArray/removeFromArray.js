1c2bda2b9aa474256bc15e8a746b8badaf7990dd
krexaim <krexaim@gmail.com>
2022-07-04 11:24:49 -0300

    testing .includes method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,22 @@
 const removeFromArray = function(array, item) {
-    //create array of arguments (1, length)
     let args = [];
     let newArray = [];
 
+    //create array of arguments (1, length)
     for (let i = 1; i < arguments.length; i++) {
-        args[i] = arguments[i];
-        }
-    for (let i = 0; i < args.length; i++) {
+        args[i - 1] = arguments[i];
+        };
+
+    //if array not in args, add that element to newArray
+    for (let i = 0; i < array.length; i++) {
         if (!array.includes(args[i])) {
-            newArray.push(array[i])
+            newArray.push(args[i])
         }
     }
     return newArray;
 }
 
-
+removeFromArray([1,2,3,4], 3)
 
 
 // Do not edit below this line
