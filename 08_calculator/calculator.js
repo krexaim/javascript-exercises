4fa42e48206635299b2352bae5c2e4cdd01f4654
krexaim <krexaim@gmail.com>
2022-07-08 16:33:01 -0300

    solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(...args) {
+	let total = 0;
+  for (arg of args) {
+    total += arg;
+  }
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  let total = args[0];
+  for (let i = 1; i < args.length; i++) {
+    total -= args[i]
+  }
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+	for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i]
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  let total = 1;
+	for (let i = num; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
