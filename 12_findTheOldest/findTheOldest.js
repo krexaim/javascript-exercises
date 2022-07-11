ec26ac346213d2992c00a39f1dd66ae555e4bf86
krexaim <krexaim@gmail.com>
2022-07-11 13:49:37 -0300

    first test case

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let ages = people.map(person => person.yearOfDeath - person.yearOfBirth);
+    let oldest = ages.indexOf(Math.max(...ages));
 
+    return people[oldest];
 };
 
 // Do not edit below this line
