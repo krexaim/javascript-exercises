b6ebf25a66d097d6b0d0c63019415278f093ece9
krexaim <krexaim@gmail.com>
2022-07-11 14:22:56 -0300

    start

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,13 @@
-const caesar = function() {
-
+const caesar = function(text, shift) {
+    let cipher = "";
+    let unicode = "";
+    let final = "";
+    for (let i = 0; i < text.length; i++) {
+        unicode = text.charCodeAt(i);
+        cipher = unicode + shift;
+        final += String.fromCharCode(cipher)
+    }
+    return final;
 };
 
 // Do not edit below this line
