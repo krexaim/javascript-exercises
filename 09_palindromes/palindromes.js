a98147359603f6a5ef395d1aaad97eb65b986f0b
krexaim <krexaim@gmail.com>
2022-07-11 10:42:55 -0300

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let alnum = str.replace(/[^0-9A-Z]+/gi,"");
+    let clean = alnum.toLowerCase();
+    let backwards = "";
 
+    for (let i = clean.length - 1; i >= 0; i--) {
+        backwards += clean[i];
+    }
+    return backwards == clean;
 };
 
 // Do not edit below this line
