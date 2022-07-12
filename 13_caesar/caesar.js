d8483ed7339e12680f0434b4f4e398a8e647aa66
krexaim <krexaim@gmail.com>
2022-07-12 10:49:10 -0300

    pick up later

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,13 +1,19 @@
 const caesar = function(text, shift) {
     let cipher = "";
-    let unicode = "";
+    let code = "";
     let final = "";
     for (let i = 0; i < text.length; i++) {
         unicode = text.charCodeAt(i);
-        cipher = unicode + shift;
-        final += String.fromCharCode(cipher)
-    }
+        if (!(code > 47 && code < 58) && // numeric (0-9)
+            !(code > 64 && code < 91) && // upper alpha (A-Z)
+            !(code > 96 && code < 123)) { // lower alpha (a-z)
+                final += text[i]
+        } else {
+            cipher = unicode + shift;
+            final += String.fromCharCode(cipher)
+        }
     return final;
+    }
 };
 
 // Do not edit below this line
